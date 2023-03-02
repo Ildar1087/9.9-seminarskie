9b6cdc892b989d41f921bc192edfc38cf0e1d9be
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-03-02 13:03:54 +0300

    s seminara 2

---
+++ 3.0Zadacha/Program.cs
@@ -0,0 +1,22 @@
+﻿/***Задача 67:**Напишите программу, 
+которая будет принимать на вход число и возвращать сумму его цифр.
+
+453 -> 12
+
+45 -> 9*/
+
+Console.Clear();
+Console.Write("введите число: ");
+int n = int.Parse(Console.ReadLine());
+int SummaChisla(int number)
+{
+  int sum = 0;
+  while (n > 0)
+  {
+    int num = n % 10;
+    n = n / 10;
+    sum = sum + num;
+  }
+  return sum;
+}
+Console.WriteLine($"{SummaChisla(n)}");

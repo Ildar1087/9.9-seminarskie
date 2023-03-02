62e94cde248bc9cd35c7dbe2342c81ca9887aecd
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-03-02 13:02:00 +0300

    rabochaya programma

---
+++ 1.0Zadacha/Program.cs
@@ -0,0 +1,51 @@
+﻿/*  *Задача 63:** Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от 1 до N.
+
+N = 5 -> "1, 2, 3, 4, 5"
+
+N = 6 -> "1, 2, 3, 4, 5, 6"  */
+
+
+
+using System;
+using static System.Console;
+
+// Clear();
+// Write("Введите N: ");
+// int n = int.Parse(ReadLine());
+// Write("Введите M: ");
+// var m = int.Parse(ReadLine());
+
+// WriteLine(PrintNumbers(n));
+
+// int PrintNumbers(int end)
+// {
+//   if (end == m)
+//   {
+//     Console.WriteLine(1);
+//     return "m";
+//   } 
+//   string s= end.ToString() + ' ' + PrintNumbers(end - 1);
+//   int summ = int.Parse(s);
+//   summ*=summ;
+//   Console.WriteLine($"{summ = int.Parse(s)} ");
+//   return (summ);
+// }
+
+
+
+// if (M == 0) return (N * (N + 1)) / 2;            // Если M равно нулю
+// else if (N == 0) return (M * (M + 1)) / 2;       // Если N равно нулю
+// else 
+//   if (M == N) return M;                       // Если M=N
+//   else if (M < N) return N + SumNumbers(M, N - 1); // Если M<N
+//   else return N + SumNumbers(M, N + 1);            // Если M>N
+// }
+
+/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30   */
+
+
